c17625c020c3cef590e0520220885194ed1252de
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-08-01 12:10:42 -0700

    update

--- curve25519-dalek/src/backend/serial/scalar_mul/vartime_double_base.rs
+++ curve25519-dalek/src/backend/serial/scalar_mul/vartime_double_base.rs
@@ -84,9 +84,9 @@ use sp1_lib::{ed25519::Ed25519AffinePoint, utils::AffinePoint};
 pub fn mul(a: &Scalar, A: &EdwardsPoint, b: &Scalar) -> EdwardsPoint {
     let A: Ed25519AffinePoint = (*A).into();
 
-    let a_bits = a.bits();
+    let a_bits = a.bits_le();
     let a_bits = a_bits.iter().map(|bit| *bit == 1).collect::<Vec<bool>>();
-    let b_bits = b.bits();
+    let b_bits = b.bits_le();
     let b_bits = b_bits.iter().map(|bit| *bit == 1).collect::<Vec<bool>>();
 
     // Note: The base point is the identity point.
